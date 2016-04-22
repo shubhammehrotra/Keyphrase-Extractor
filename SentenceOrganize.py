1294ee7d5d856f40a9555b1f6f274d343edf9a2b
shubhammehrotra94 <shubhammehrotra94@gmail.com>
2016-04-22 15:01:09 +0100

    updates

--- SentenceOrganize.py
+++ SentenceOrganize.py
@@ -1,12 +1,13 @@
 import os
 import csv
-variable = ['Baby', 'Electronics']
+variable = ['Electronics', 'Baby']
 for v in variable:
     fileDirectory = 'C:/Python34/Keyphrase/output/' + v + '/'
     inputFilePath = "C:/Users/shubham_15294/Downloads/"
     fileName = v
     for fn in os.listdir(fileDirectory):
         f = open(fileDirectory + fn,'r')
+       
         count = 0
         s = fn.strip(".txt")
         newFile = open('C:/Python34/Keyphrase/output/Sentences/' + v + '/' + s + ' Sentence.txt', 'w')
@@ -14,14 +15,18 @@ for v in variable:
             count = count + 1
             fileRead = open(inputFilePath + fileName + ".csv","r")
             if (count != -1):
+                cnt = 0
                 with fileRead as tsvfile:
                     tsvreader = csv.reader(tsvfile)
                     for lines in tsvreader:
+                        
                         tag = int(lines[2])
                         if (tag == 1):
+                            
                             sentence = lines[1]
                             a = str(text).strip()
                             if a in sentence:
-                                print(fn + ' ' + a + '     ' + sentence)
-                                newFile.write(sentence + '\n')
+                                cnt = cnt + 1
+                                newFile.write(sentence + '\n' + '\n')
+    print(v + ' Done !')
 print('Done')
